209a7078f3e26b3bb8f21e70497e405b5c29db4d
MujPrvniGutHib <xivludvik@gmail.com>
2026-05-01 16:46:23 +0200

    komit

---
+++ src/main/java/osu/cz/swi1/Swi1Application.java
@@ -0,0 +1,13 @@
+package osu.cz.swi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/osu/cz/swi1/Swi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package osu.cz.swi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
